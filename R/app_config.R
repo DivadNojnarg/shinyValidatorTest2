85c2c8865ee953f90a7007272b46513924b92c60
DivadNojnarg <dgranjon@ymail.com>
2022-04-25 15:53:29 +0200

    update

--- R/app_config.R
+++ R/app_config.R
@@ -1,28 +1,28 @@
 #' Access files in the current app
-#' 
-#' NOTE: If you manually change your package name in the DESCRIPTION, 
-#' don't forget to change it here too, and in the config file. 
+#'
+#' NOTE: If you manually change your package name in the DESCRIPTION,
+#' don't forget to change it here too, and in the config file.
 #' For a safer name change mechanism, use the `golem::set_golem_name()` function.
-#' 
+#'
 #' @param ... character vectors, specifying subdirectory and file(s) 
-#' within your package. The default, none, returns the root of the app. 
-#' 
+#' within your package. The default, none, returns the root of the app.
+#'
 #' @noRd
-app_sys <- function(...){
+app_sys <- function(...) {
   system.file(..., package = "shinyValidatorTest2GA")
 }
 
 
 #' Read App Config
-#' 
+#'
 #' @param value Value to retrieve from the config file. 
-#' @param config GOLEM_CONFIG_ACTIVE value. If unset, R_CONFIG_ACTIVE. 
+#' @param config GOLEM_CONFIG_ACTIVE value. If unset, R_CONFIG_ACTIVE.
 #' If unset, "default".
 #' @param use_parent Logical, scan the parent directory for config file.
 #' 
 #' @noRd
 get_golem_config <- function(
-  value, 
+  value,
   config = Sys.getenv(
     "GOLEM_CONFIG_ACTIVE", 
     Sys.getenv(
@@ -31,13 +31,12 @@ get_golem_config <- function(
     )
   ), 
   use_parent = TRUE
-){
+) {
   config::get(
-    value = value, 
-    config = config, 
+    value = value,
+    config = config,
     # Modify this if your config file is somewhere else:
-    file = app_sys("golem-config.yml"), 
+    file = app_sys("golem-config.yml"),
     use_parent = use_parent
   )
 }
-
